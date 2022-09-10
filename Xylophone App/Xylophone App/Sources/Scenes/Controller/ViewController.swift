59fbcf3a5f9dade113af27262bca5370ae3e5eb6
Georgiy <33875528+georg1856@users.noreply.github.com>
2022-09-10 16:19:57 +1000

    Create simple logic methods

--- Xylophone App/Xylophone App/Sources/Scenes/Controller/ViewController.swift
+++ Xylophone App/Xylophone App/Sources/Scenes/Controller/ViewController.swift
@@ -6,14 +6,27 @@
 //
 
 import UIKit
+import AVFoundation
+
 
 class ViewController: UIViewController {
+    
+    var player: AVAudioPlayer!
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
+    
+    @IBAction func keyPressed(_ sender: UIButton) {
+        let nameSound = sender.titleLabel?.text ?? "nil"
+        playSound(for: nameSound)
+    }
+    
+    func playSound(for music: String) {
 
-
+        let url = Bundle.main.url(forResource: music, withExtension: "wav")
+        player = try! AVAudioPlayer(contentsOf: url!)
+        player.play()
+    }
 }
-
