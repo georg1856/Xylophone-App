6a12241f3bae93d39280060da7312deacec34e83
Georgiy <33875528+georg1856@users.noreply.github.com>
2022-09-10 16:22:12 +1000

    Create DispatchQ

--- Xylophone App/Xylophone App/Sources/Scenes/Controller/ViewController.swift
+++ Xylophone App/Xylophone App/Sources/Scenes/Controller/ViewController.swift
@@ -21,6 +21,11 @@ class ViewController: UIViewController {
     @IBAction func keyPressed(_ sender: UIButton) {
         let nameSound = sender.titleLabel?.text ?? "nil"
         playSound(for: nameSound)
+        
+        sender.alpha = 0.5
+        DispatchQueue.main.asyncAfter(deadline: .now() + 0.2) {
+            sender.alpha = 1.0
+        }
     }
     
     func playSound(for music: String) {
